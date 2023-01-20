40b7af9391bcaff9db0f394af733cb74b675085e
lillian-jewel <lillian.j.arcos@gmail.com>
2023-01-20 14:18:16 -0600

    JS with HTML

---
+++ js/externalJS.js
@@ -0,0 +1,25 @@
+"use strict";
+
+console.log("Hello World")
+
+// alert("Hi Reader")
+
+// var myName = "Lilly";
+
+// alert("Hey this is " + myName);
+
+// alert(2+2);
+
+// alert(true && false);
+
+// alert(2+2-5-(5*5));
+
+//Confirm true or false
+var likedLunch = confirm("Lunch today?");
+
+alert("You told me that it was " + likedLunch + "that you had lunch");
+
+//Prompt
+var whatDidYouHaveForLunch = prompt("You told me that it was " + likedLunch + " that you had lunch");
+
+alert("You told me that it was " + whatDidYouHaveForLunch + "that you had lunch");
